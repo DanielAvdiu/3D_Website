c308ec5bffc1391ce5d6caf2755283d01a3be31a
Daniel-0507 <davdiu20@epoka.edu.al>
2023-09-30 19:46:31 -0400

    chnages

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -27,9 +27,10 @@ const Computers = ({isMobile}) => {
 
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.4 : 0.6}
+        scale={isMobile ? 0.5 : 0.75}
         position={isMobile ? [0, -3, -2.2]:[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
+        style={{ userSelect: 'none' }}
       />
 
     </mesh>
