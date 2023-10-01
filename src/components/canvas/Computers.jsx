f075a6e9dca6a71fb623d52a84a261946a093553
Daniel-0507 <davdiu20@epoka.edu.al>
2023-09-30 22:35:48 -0400

    changed a small detail

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -76,7 +76,7 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-          enableRotate={false}
+          autoRotate={true}
           />
 
           {/* It limits the movement of the model.
